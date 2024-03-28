0d6e54c088311c167697323822a6b06c8efd31c1
Bannasorn Paspanthong <bannasornpaspanthong2023@u.northwestern.edu>
2024-03-27 20:06:21 -0500

    create version1 of data_generation function

---
+++ data_generation.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+from sklearn.datasets import make_classification
+
+def data_generation(
+    ratio: float,
+    n_samples: int,
+    n_features: int = 20,
+    n_informative: int = 7,
+    random_state: int = 7,
+    target_randomness: float = 0.05
+) -> pd.DataFrame:
+    if ratio <= 0 or ratio >= 1:
+        raise ValueError("ratio but be between 0 and 1")
+    
+    # generate binary classification data
+    X, y = make_classification(weights=ratio,
+                               n_samples=n_samples,
+                               n_features=n_features,
+                               n_informative=n_informative,
+                               random_state=random_state,
+                               flip_y=target_randomness)
+    
+    # convert data into dataframe format
+    df = pd.DataFrame(X, columns=[f"feat_{i + 1}" for i in range(n_features)])
+    df["target"] = y
+    
+    return df
