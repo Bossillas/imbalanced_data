c6a264106ab74cdcf90bdb5a8fd1831f5489b587
Bannasorn Paspanthong <bannasornpaspanthong2023@u.northwestern.edu>
2024-06-07 15:33:20 -0500

    add data generation section to pipeline:

---
+++ pipeline.py
@@ -0,0 +1,25 @@
+import yaml
+from sklearn.model_selection import train_test_spli
+import data_generation as dg
+
+# read config
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+    
+def pipeline(cfg: dict):
+    
+    # generate data
+    df = dg.data_generation(cfg["data"]["ratio"],
+                            cfg["data"]["n_samples"],
+                            cfg["data"]["n_features"],
+                            cfg["data"]["n_informative"],
+                            cfg["data"]["random_state"],
+                            cfg["data"]["target_randomness"])
+    
+    df = dg.fix_imbalance(df, 
+                          cfg["data"]["fix_imbalance_strategy"],
+                          cfg["data"]["random_state"])
+
+
+if __name__ == "__main__":
+    pipeline(config)
